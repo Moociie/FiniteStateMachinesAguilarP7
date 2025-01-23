47db65231728b5d3266c4bd762b0d972eda0c0a3
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-01-23 15:25:04 -0800

    Creating and Using a state class
    part finished, moving onto patrolling  the perimeter

---
+++ FiniteStateMachinesP7Aguilar/Assets/Scripts/GameEnvironment.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment : MonoBehaviour
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(
+                    GameObject.FindGameObjectsWithTag("Checkpoints"));
+            }
+            return instance;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
