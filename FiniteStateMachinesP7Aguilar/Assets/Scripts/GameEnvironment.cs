6a0843af8c944908c6a762cdb852cbffa032ed01
Kimberly Aguilar <145504527+Moociie@users.noreply.github.com>
2025-01-29 15:26:59 -0800

    finished
    just need recording

--- FiniteStateMachinesP7Aguilar/Assets/Scripts/GameEnvironment.cs
+++ FiniteStateMachinesP7Aguilar/Assets/Scripts/GameEnvironment.cs
@@ -1,35 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public sealed class GameEnvironment : MonoBehaviour
-{
+public class GameEnvironment {
+
     private static GameEnvironment instance;
     private List<GameObject> checkpoints = new List<GameObject>();
+
     public List<GameObject> Checkpoints { get { return checkpoints; } }
 
-    public static GameEnvironment Singleton
-    {
-        get
-        {
-            if (instance == null)
-            {
+    public static GameEnvironment Singleton {
+
+        get {
+
+            if (instance == null) {
+
                 instance = new GameEnvironment();
-                instance.Checkpoints.AddRange(
-                    GameObject.FindGameObjectsWithTag("Checkpoints"));
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
             }
+
             return instance;
         }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
